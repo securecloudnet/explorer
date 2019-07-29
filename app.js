e2e2dcc2f731cf795a6ae20fd2a3f6063aad7d2b
ZioFabry <ziofabry@hotmail.com>
2019-07-29 19:55:33 +0000

    latest version

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ if (settings.heavy != true) {
   bitcoinapi.setAccess('only', ['getinfo', 'getstakinginfo', 'getnetworkhashps', 'getdifficulty', 'getconnectioncount',
     'getblockcount', 'getblockhash', 'getblock', 'getrawtransaction','getmaxmoney', 'getvote',
     'getmaxvote', 'getphase', 'getreward', 'getnextrewardestimate', 'getnextrewardwhenstr',
-    'getnextrewardwhensec', 'getsupply', 'gettxoutsetinfo', 'masternode']);
+    'getnextrewardwhensec', 'getsupply', 'gettxoutsetinfo', 'masternode', 'getpeerinfo']);
 }
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
